64bafbaf0d088a040c6de4ad790783d5c1dafbf2
robBowes <32183505+robBowes@users.noreply.github.com>
2018-05-07 14:34:42 -0400

    adding eslint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Route, BrowserRouter, Link } from 'react-router-dom' 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+
+
+        <BrowserRouter>
+        <div>
+        <Route exact path='/' render={renderAllItems} />
+        </div>
+        </BrowserRouter>
       </div>
     );
   }
