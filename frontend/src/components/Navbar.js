f666092a70fd2625fc02546b21512fb9e82375d1
robBowes <32183505+robBowes@users.noreply.github.com>
2018-05-07 17:07:27 -0400

    add grid to navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,7 +7,7 @@ import React, {Component} from 'react';
 class Navbar extends Component {
     render() {
         return (
-            <div>
+            <div className="navBar">
                 <img src="" alt="the bay logo"/>
             </div>
         );
