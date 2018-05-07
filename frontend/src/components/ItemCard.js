c8c5001e970b98a7e7682931c3f7e9ca65973a82
robBowes <32183505+robBowes@users.noreply.github.com>
2018-05-07 17:32:38 -0400

    unknown changes

--- frontend/src/components/ItemCard.js
+++ frontend/src/components/ItemCard.js
@@ -7,15 +7,11 @@ class ItemCard extends Component {
     render() {
         return (
             <div>
-<<<<<<< HEAD
                 <div>header<button>close pane</button></div>
                 <div>image</div>
                 <div>Details text</div>
                 Other Text/description
                 <button>BUY</button>
-=======
-
->>>>>>> 43f9c98098eb960e12aba3b9dc640939c3e45ea0
             </div>
         );
     }
