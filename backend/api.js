83896a36c36676bdd4aa61b4286d9837d8abc978
Yazid Mehenni <yazid129@gmail.com>
2018-05-07 16:42:13 -0400

    added api.js skeleton, hello world endpoint removed

--- backend/api.js
+++ backend/api.js
@@ -1,19 +1,76 @@
-const alibay = require('./alibay')
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const alibay = require("./alibay");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-app.get('/itemsBought', (req, res) => {
-    let uid = req.query.uid;
-    res.send(JSON.stringify(alibay.getItemsBought(uid)));
+app.use(bodyParser.raw({ type: "*/*" }));
+
+app.get("/itemsBought", (req, res) => {
+  let uid = req.query.uid;
+  res.send(JSON.stringify(alibay.getItemsBought(uid)));
 });
 
-app.get('/helloWorld', (req,res) => {
-    res.send("<h1>HELLO WORLD</h1>")
-})
+app.post("/login", (req, res) => {
+  let body = req.body.toString();
+  let parsedBody = JSON.parse(body);
+  let username = parsedBody.username;
+  let password = parsedBody.password;
+  res.send(JSON.stringify(alibay.login(username, password)));
+});
 
-app.get('/', (req, res) => {
-    res.send("<h1>ALIBAY</h1>")
-})
+app.post("/register", (req, res) => {
+  let body = req.body.toString();
+  let parsedBody = JSON.parse(body);
+  let username = parsedBody.username;
+  let password = parsedBody.password;
+  res.send(JSON.stringify(alibay.register(username, password)));
+});
 
-app.listen(4000, () => console.log('Listening on port 4000!'))
+app.get("/allItems", (req, res) => {
+  res.send(JSON.stringify(alibay.allItems()));
+});
+
+app.post("/item", (req, res) => {
+  let body = req.body.toString();
+  let parsedBody = JSON.parse(body);
+  let itemId = parsedBody.itemId;
+  res.send(JSON.stringify(alibay.item(itemId)));
+});
+
+app.post("/buy", (req, res) => {
+  let body = req.body.toString();
+  let parsedBody = JSON.parse(body);
+  let itemId = parsedBody.itemId;
+  let quantity = parsedBody.quantity;
+  res.send(JSON.stringify(alibay.buy(itemId, quantity)));
+});
+
+app.post("/search", (req, res) => {
+  let body = req.body.toString();
+  let parsedBody = JSON.parse(body);
+  let query = parsedBody.itemId;
+  res.send(JSON.stringify(alibay.search(query)));
+});
+
+app.put("/addItem", (req, res) => {
+  let body = req.body.toString();
+  let parsedBody = JSON.parse(body);
+  let itemTitle = parsedBody.itemTitle;
+  let itemDescription = parsedBody.itemDescription;
+  let quantity = parsedBody.quantity;
+  let sellerId = parsedBody.sellerId;
+  res.send(
+    JSON.stringify(
+      alibay.addItem(itemTitle, itemDescription, quantity, sellerId)
+    )
+  );
+});
+
+app.post("/user", (req, res) => {
+  let body = req.body.toString();
+  let parsedBody = JSON.parse(body);
+  let userId = parsedBody.userId;
+  res.send(JSON.stringify(alibay.user(userId)));
+});
+
+app.listen(4000, () => console.log("Listening on port 4000!"));
