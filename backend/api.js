603f01c27da2e88ab5a54bcdcd74af4129e4a02a
Yazid Mehenni <yazid129@gmail.com>
2018-05-07 15:07:02 -0400

    initialize backend packages, hello world GET

--- backend/api.js
+++ backend/api.js
@@ -1,10 +1,19 @@
 const alibay = require('./alibay')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
 app.get('/itemsBought', (req, res) => {
     let uid = req.query.uid;
     res.send(JSON.stringify(alibay.getItemsBought(uid)));
 });
+
+app.get('/helloWorld', (req,res) => {
+    res.send("<h1>HELLO WORLD</h1>")
+})
+
+app.get('/', (req, res) => {
+    res.send("<h1>ALIBAY</h1>")
+})
 
 app.listen(3000, () => console.log('Listening on port 3000!'))
