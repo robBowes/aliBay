8ee204683379f2a98cec50c02ec1341dfab6b009
phillips-jordan <jordan.phillips.nb@gmail.com>
2018-05-07 16:48:10 -0400

    placeholder functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,38 @@ import logo from './logo.svg';
 import {Route, BrowserRouter, Link} from 'react-router-dom';
 import './App.css';
 
+renderAllItems = () => {
+  //This function will render <Item/> components for each Item in item array
+}
+
+renderItemCard = () => {
+  //This function will render an <Item/> card component when a user
+  // click on a specific item to View
+}
+
+renderNavBar = () => {
+  //This function is responsible for rendering the navbar at top
+  //of screen
+}
+renderFooter = () => {
+  //this function renders footer at all times (almost all times?)
+}
+
+
+
+renderLogin = () => {
+  //This is the first function that renders when a user lands
+  //other than the top anchored navbar
+  //on page. Will render <Login/> Component
+}
+
+renderRegisterUser = () => {
+  //This function will render the <AccountCreation/> card when a
+  //user click the Create Account button on the login page
+}
+
+
+
 class App extends Component {
   helloWorld = () => {
     fetch('/helloWorld').then((res)=>res.text()).then((data)=>console.log(data));
