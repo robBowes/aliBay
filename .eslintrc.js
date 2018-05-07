557693ab0dde0cff688b87894d87c7e936bf248d
robBowes <32183505+robBowes@users.noreply.github.com>
2018-05-07 14:44:16 -0400

    eslint updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,19 @@
 module.exports = {
-    "extends": "google"
+    "extends": "google",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "env" : {
+        "browser": true,
+        "node" : true,
+    },
+    "rules": {
+        "semi": ["error", "always"],
+        "require-jsdoc" : "off",
+        "linebreak-style":"off"
+    }
 };
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
-import { Route, BrowserRouter, Link } from 'react-router-dom' 
+import {Route, BrowserRouter, Link} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+
 
 
         <BrowserRouter>
