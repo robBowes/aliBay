cce48be561b4d9c8b172c143b0f140d6a7214017
robBowes <32183505+robBowes@users.noreply.github.com>
2018-05-07 19:16:17 -0400

    working on search

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -4,17 +4,25 @@
 import React, {Component} from 'react';
 
 class Search extends Component {
+    constructor() {
+        super();
+        this.state = {
+            searchContents: '',
+        };
+    }
     handleChange = (event)=>{
-
+        event.preventDefault();
+        this.setState({searchContents: event.target.value});
     }
     handleSubmit = (event)=>{
         event.preventDefault();
+        
     }
     render() {
         return (
             <div className="search">
                 <form>
-                    <input type='text' onChange={this.handleChange}/>
+                    <input type='text' value={this.state.searchContents} onChange={this.handleChange}/>
                     <input type='submit' onSubmit={this.handleSearch}/>
                 </form>
             </div>
