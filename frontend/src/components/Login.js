5c0947a82bda632410714ab202e41a0fb1cf284f
robBowes <32183505+robBowes@users.noreply.github.com>
2018-05-08 14:36:21 -0400

    transition for login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,6 @@
 /**
- * An entry point to the app where a user can log in
- */
+* An entry point to the app where a user can log in
+*/
 import React, {Component} from 'react';
 
 class Login extends Component {
@@ -9,6 +9,7 @@ class Login extends Component {
         this.state = {
             usernameValue: 'bob',
             passwordValue: 'test',
+            classLogin: 'hidden',
         };
     }
     handleSubmit = (event) => {
@@ -16,49 +17,54 @@ class Login extends Component {
         fetch('/login', {
             method: 'POST',
             credentials: 'same-origin',
-            body: JSON.stringify({username: this.state.usernameValue, password: this.state.passwordValue}),
-        })
-        .then((response)=>response.text())
-        .then((response)=>{
-            console.log(response)
-            let parsedResponse = JSON.parse(response);
-            if (parsedResponse.status === true) {
-                // alert(parsedResponse.reason);
-                this.props.updateUserInfo(parsedResponse);
-            } else {
-                alert(parsedResponse.reason);
-            }
-        });
-    }
-    handleChange = (event) => {
-        this.setState({[event.target.name]: event.target.value});
-    }
-    render() {
-        return (
-            <div className='login'>
+            body: JSON.stringify({username: this.state.usernameValue,
+                password: this.state.passwordValue}),
+            })
+            .then((response)=>response.text())
+            .then((response)=>{
+                console.log(response);
+                let parsedResponse = JSON.parse(response);
+                if (parsedResponse.status === true) {
+                    // alert(parsedResponse.reason);
+                    this.props.updateUserInfo(parsedResponse);
+                } else {
+                    alert(parsedResponse.reason);
+                }
+            });
+        }
+        handleChange = (event) => {
+            this.setState({[event.target.name]: event.target.value});
+        }
+        componentDidMount() {
+            this.setState({classLogin: 'login'});
+        }
+        render() {
+            return (
+                <div className={this.state.classLogin} >
                 <div>LOG IN HEADER</div>
-                    <form onSubmit={this.handleSubmit}>
-                    LOGIN:<br/>
-                    USERNAME<br/>
-                    <input
-                    type='text'
-                    id='username'
-                    name='usernameValue'
-                    onChange={this.handleChange}
-                    value={this.state.usernameValue} />
-                    <br/>PASSWORD<br/>
-                    <input
-                    type='password'
-                    id='password'
-                    name='passwordValue'
-                    onChange={this.handleChange}
-                    value= {this.state.passwordValue} />
-                    <br/>
-                    <input type='submit'/>
-                    </form>
-            </div>
-        );
+                <form onSubmit={this.handleSubmit}>
+                LOGIN:<br/>
+                USERNAME<br/>
+                <input
+                type='text'
+                id='username'
+                name='usernameValue'
+                onChange={this.handleChange}
+                value={this.state.usernameValue} />
+                <br/>PASSWORD<br/>
+                <input
+                type='password'
+                id='password'
+                name='passwordValue'
+                onChange={this.handleChange}
+                value= {this.state.passwordValue} />
+                <br/>
+                <input type='submit'/>
+                </form>
+                </div>
+            );
+        }
     }
-}
-
-export default Login;
+    
+    export default Login;
+    
