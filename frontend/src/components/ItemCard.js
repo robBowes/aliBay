c536fdbc2912177957e73cf45f53f22d61017dd8
robBowes <32183505+robBowes@users.noreply.github.com>
2018-05-08 17:20:46 -0400

    item card is clickable

--- frontend/src/components/ItemCard.js
+++ frontend/src/components/ItemCard.js
@@ -1,6 +1,6 @@
 /**
- * Shown on results page with small image and item description
- */
+* Shown on results page with small image and item description
+*/
 import React, {Component} from 'react';
 import styled from 'styled-components';
 import { Route, BrowserRouter, Link } from "react-router-dom";
@@ -10,22 +10,18 @@ margin: 0.5rem 0;
 `;
 
 class ItemCard extends Component {
-    render() {
-        return <Card className="card border-primary mb2">
-            <h5 className="card-header">
-              {" "}
-              {this.props.item.itemName}{" "}
-            </h5>
-            <h6> {this.props.item.itemDescription} </h6>
-            <p>Price $ {this.props.item.price} </p>
-            <p>Left in stock: {this.props.item.quantity} </p>
-              <Link to={"/item/" + this.props.item.itemId}>
-            <button>
-              CLICK
-            </button>
-              </Link>
-          </Card>;
-    }
+  render() {
+    return <Card className="card border-primary mb2">
+    <Link to={"/item/" + this.props.item.itemId}>
+    <h5 className="card-header">
+    {" "}
+    {this.props.item.itemName}{" "}
+    </h5>
+    <h6> {this.props.item.itemDescription} </h6>
+    <p>Price $ {this.props.item.price} </p>
+    </Link>
+    </Card>;
+  }
 }
 
 export default ItemCard;
