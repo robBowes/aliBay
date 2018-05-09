7095a67b5695eeafd7af00c489d1973c4c73dcb5
robBowes <32183505+robBowes@users.noreply.github.com>
2018-05-09 18:36:48 -0400

    navbar dropdown

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -15,60 +15,55 @@ const disabledBtn = 'btn btn-info disabled';
 
 class Navbar extends Component {
   render() {
-    return <div
-    className="navBar navbar navbar-dark navbar-expand-lg  bg-dark">
+    return <nav
+    className="navBar navbar navbar-expand-lg navbar-dark bg-dark avbar-toggleable-md">
 
+    <Link className="navbar-brand" style={{height: '100%'}} to="/">
     <img src="/keybaylogo.png" alt="the bay logo" style={{height: '90%'}}/>
-
-    <Link to="/">
-
-    {/* <a className="navbar-brand">Key-Bay</a> */}
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" style={{}}>
-    <span className="navbar-toggler-icon"></span>
-    </button>
     </Link>
-    <div className='navbar-collapse collapse show'>
 
-    <ul className=" navbar-nav ml-auto">
+    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" style={{}}>
+    <span className="navbar-toggler-icon "></span>
+    </button>
+
+    <div className='navbar-collapse collapse' id="navbarColor02">
+    <ul className="navbar-nav ml-auto">
+
     <li
-    style={{height: '100%'}}
-    // className={this.props.loggedIn ? btn : disabledBtn}
-    className="nav-item "
+    // style={{height: '100%'}}
+    className="nav-item active"
     onClick={this.props.toggleSellItem} >
-
     <Link
-    // className={'nav-link ' + this.props.loggedIn?'disabled':''}
     className="nav-link"
-
+    href='#'
     to="/">
     Sell Item
     </Link>
     </li>
+
     <li
-    style={{height: '100%'}}
-    className="nav-item "
-    >
+    // style={{height: '100%'}}
+    className="nav-item" >
     <Link
     className="nav-link"
+    href='#'
     to={!this.props.loggedIn?'/':'/profile/user'+this.props.userId}>
     My Profile
     </Link>
     </li>
+
     <li
     className="nav-item "
-    style={{height: '100%'}}
+    // style={{height: '100%'}}
     onClick={this.props.handleLogout}>
-    <Link className="nav-link" to="/">
+    <Link className="nav-link" href='#' to="/">
     Log out
     </Link>
     </li>
+
     </ul>
-    {/* <form className="form-inline my-2 my-lg-0">
-    <input className="form-control mr-sm-2" type="text" placeholder="Search" />
-    <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-    </form> */}
     </div>
-    </div>;
+    </nav>;
   }
 }
 
