5eae2afea6af6a65398839c30f3929ed4e667199
robBowes <32183505+robBowes@users.noreply.github.com>
2018-05-09 10:03:39 -0400

    eslint fix

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -4,13 +4,8 @@ module.exports = {
     "parserOptions": {
         "ecmaVersion": 7,
         "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true
-        }
     },
-    "plugins": ["react"],
     "env" : {
-        "browser": true,
         "node" : true,
         "es6" :true,
     },
--- backend/alibay.js
+++ backend/alibay.js
@@ -1,5 +1,5 @@
-const assert = require("assert");
-const _ = require("lodash");
+const assert = require('assert');
+const _ = require('lodash');
 
 function genUID() {
   return Math.floor(Math.random() * 100000000);
@@ -7,66 +7,66 @@ function genUID() {
 
 let users = {
   10000001: {
-    username: "bob",
-    password: "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3",
+    username: 'bob',
+    password: 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3',
     itemsListed: [23423],
     transactions: [23424234234, 2342342],
-    sessionId: 00000000002,
+    sessionId: 1,
     signupDate: 12546845,
-    description: "this is my profile, it is a string",
-    lastLoginDate: 175654646
+    description: 'this is my profile, it is a string',
+    lastLoginDate: 175654646,
   },
   10000002: {
-    username: "glob",
-    password: "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3",
+    username: 'glob',
+    password: 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3',
     itemsListed: [23423],
     transactions: [23424234234, 2342342],
-    sessionId: 00000000002,
+    sessionId: 1,
     signupDate: 12546845,
-    description: "this is my profile, it is a string",
-    lastLoginDate: 175654646
-  }
+    description: 'this is my profile, it is a string',
+    lastLoginDate: 175654646,
+  },
 };
 
 let items = {
   20000001: {
-    itemDescription: "sample",
-    itemName: "some name",
+    itemDescription: 'sample',
+    itemName: 'some name',
     price: 12312.12,
     quantity: 10,
     listDate: 713649819,
-    sellerId: 10000002
+    sellerId: 10000002,
   },
   20000002: {
-    itemName: "Red Bike",
+    itemName: 'Red Bike',
     itemDescription:
-      "works great 2015 model selling because I bought a new one. I have 2 for sale",
-    sellerName: "Bobert Dobert",
+      'works great 2015 model selling because I bought a new one. I have 2 for sale',
+    sellerName: 'Bobert Dobert',
     price: 60,
     quantity: 2,
     sellerId: 869868,
-    listDate: 1519216899934
+    listDate: 1519216899934,
   },
   20000003: {
-    itemName: "2017 Corvette Z06",
+    itemName: '2017 Corvette Z06',
     itemDescription:
-      "My midlife crisis is over sweet car but now I have to live a boring life also I got a DUI",
-    sellerName: "Aubrey Drake Graham Jr",
+      'My midlife crisis is over sweet car but now I have to live a boring life also I got a DUI',
+    sellerName: 'Aubrey Drake Graham Jr',
     price: 50000,
     quantity: 1,
     sellerId: 896070,
-    listDate: 1519211899934
+    listDate: 1519211899934,
   },
   20000004: {
-    itemName: "left shoe",
+    itemName: 'left shoe',
     itemDescription:
-      "Selling because I lost the right one its an original Air Yeezy worth $4000 for the pair so this is a bargain",
-    sellerName: "Slim Jimmy",
+      'Selling because I lost the right one its an original Air Yeezy worth $4000 for the pair so this is a bargain',
+    sellerName: 'Slim Jimmy',
     price: 300,
     quantity: 1,
     sellerId: 658791,
-    listDate: 1519211809934
-  }
+    listDate: 1519211809934,
+  },
 };
 
 let transactions = {
@@ -75,55 +75,55 @@ let transactions = {
     buyerId: 10000002,
     itemId: 20000002,
     quantity: 2,
-    price: 123.5
-  }
+    price: 123.5,
+  },
 };
 
 let login = (username, password, sessionId) => {
   username = username.toLowerCase();
-  let userId = _.findKey(users, x => x["username"] === username);
+  let userId = _.findKey(users, (x) => x['username'] === username);
   if (!userId) {
     return {
       status: false,
-      reason: "Username does not exist"
+      reason: 'Username does not exist',
     };
   }
-  if (users[userId]["password"] === password) {
-    users[userId]["sessionId"] = sessionId;
+  if (users[userId]['password'] === password) {
+    users[userId]['sessionId'] = sessionId;
     return {
       status: true,
       sessionId: sessionId,
       userId: userId,
-      reason: "Login Successful!"
+      reason: 'Login Successful!',
     };
   }
 
-  if (users[userId]["password"] !== password) {
+  if (users[userId]['password'] !== password) {
     return {
       status: false,
-      reason: "Incorrect password."
+      reason: 'Incorrect password.',
     };
   }
 
   return {
     status: false,
-    reason: "Unknown Error"
+    reason: 'Unknown Error',
   };
 };
 
 let register = (username, password, sessionId) => {
   username = username.toLowerCase();
-  let usernames = _.map(users, "username");
-  let userDoesExist = usernames.some(x => x === username);
+  let usernames = _.map(users, 'username');
+  let userDoesExist = usernames.some((x) => x === username);
   let keys = Object.keys(users);
-  let parsedKeys = keys.map(x => parseInt(x));
+  let parsedKeys = keys.map((x) => parseInt(x));
   let maxKey = Math.max(...parsedKeys);
   let userId = maxKey + 1;
 
   if (userDoesExist) {
     return {
       status: false,
-      reason: "Username already exists!"
+      reason: 'Username already exists!',
     };
   }
 
@@ -135,101 +135,101 @@ let register = (username, password, sessionId) => {
       transactions: [],
       sessionId: sessionId,
       signupDate: Date.now(),
-      description: "",
-      lastLoginDate: Date.now()
+      description: '',
+      lastLoginDate: Date.now(),
     };
     console.log(users);
     return {
       status: true,
       sessionId: sessionId,
       userId: userId,
-      reason: "Registration Successful"
+      reason: 'Registration Successful',
     };
   }
 
   return {
     status: false,
-    reason: "Unknown Error!"
+    reason: 'Unknown Error!',
   };
 };
 
 let allItems = () => {
   return {
     status: true,
-    content: items
+    content: items,
   };
 };
 
-let item = itemId => {
+let item = (itemId) => {
   if (!items[itemId]) {
     return {
       status: false,
-      reason: "itemId does not exist!"
+      reason: 'itemId does not exist!',
     };
   }
   return {
     status: true,
-    content: items[itemId]
+    content: items[itemId],
   };
 };
 
 let buy = (itemId, quantity, sessionId) => {
-  let sessions = _.map(users, "sessionId");
-  let sessionDoesExist = sessions.some(x => x === sessionId);
+  let sessions = _.map(users, 'sessionId');
+  let sessionDoesExist = sessions.some((x) => x === sessionId);
 
   if (!items[itemId]) {
     return {
       status: false,
-      reason: "Invalid itemId"
+      reason: 'Invalid itemId',
     };
   }
 
   if (!sessionDoesExist) {
     return {
       status: false,
-      reason: "Invalid sessionId"
+      reason: 'Invalid sessionId',
     };
   }
 
   let keys = Object.keys(transactions);
-  let parsedKeys = keys.map(x => parseInt(x));
+  let parsedKeys = keys.map((x) => parseInt(x));
   let maxKey = Math.max(...parsedKeys);
   let transactionId = maxKey + 1;
-  let sellerId = items[itemId]["sellerId"];
-  let buyerId = _.findKey(users, x => x["sessionId"] === sessionId);
-  let price = items[itemId]["price"];
+  let sellerId = items[itemId]['sellerId'];
+  let buyerId = _.findKey(users, (x) => x['sessionId'] === sessionId);
+  let price = items[itemId]['price'];
 
   transactions[transactionId] = {
     sellerId,
     buyerId,
     itemId,
     quantity,
-    price
+    price,
   };
 
-  items[itemId]["quantity"] = items[itemId]["quantity"] - 1;
-  users[buyerId]["transactions"] = users[buyerId]["transactions"].concat(
+  items[itemId]['quantity'] = items[itemId]['quantity'] - 1;
+  users[buyerId]['transactions'] = users[buyerId]['transactions'].concat(
     transactionId
   );
-  users[sellerId]["transactions"] = users[sellerId]["transactions"].concat(
+  users[sellerId]['transactions'] = users[sellerId]['transactions'].concat(
     transactionId
   );
 
   return {
     status: true,
-    reason: "purchase successful"
+    reason: 'purchase successful',
   };
 };
 
-let search = query => {
-  let allItems = { ...items };
-  let filteredItems = _.pickBy(items, x =>
+let search = (query) => {
+  let allItems = {...items};
+  let filteredItems = _.pickBy(items, (x) =>
     x.itemName.toLowerCase().includes(query.toLowerCase())
   );
 
   return {
     status: true,
-    content: filteredItems
+    content: filteredItems,
   };
 };
 
@@ -242,24 +242,24 @@ let addItem = (
   sessionId
 ) => {
   let keys = Object.keys(items);
-  let parsedKeys = keys.map(x => parseInt(x));
+  let parsedKeys = keys.map((x) => parseInt(x));
   let maxKey = Math.max(...parsedKeys);
   let itemId = maxKey + 1;
-  let sessions = _.map(users, "sessionId");
-  let sessionDoesExist = sessions.some(x => x === sessionId);
+  let sessions = _.map(users, 'sessionId');
+  let sessionDoesExist = sessions.some((x) => x === sessionId);
 
   if (!users[sellerId]) {
     return {
       status: false,
-      reason: "Invalid sellerId"
+      reason: 'Invalid sellerId',
     };
   }
-  let sellerName = users[sellerId]["username"];
+  let sellerName = users[sellerId]['username'];
 
   if (!sessionDoesExist) {
     return {
       status: false,
-      reason: "Invalid sessionId"
+      reason: 'Invalid sessionId',
     };
   }
 
@@ -269,35 +269,35 @@ let addItem = (
     quantity,
     sellerId,
     sellerName,
-    price
+    price,
   };
   return {
     status: true,
-    reason: "Item Successfully Listed!"
+    reason: 'Item Successfully Listed!',
   };
 };
 
 let user = (userId, sessionId) => {
-  let sessions = _.map(users, "sessionId");
-  let sessionDoesExist = sessions.some(x => x === sessionId);
+  let sessions = _.map(users, 'sessionId');
+  let sessionDoesExist = sessions.some((x) => x === sessionId);
 
   if (!(userId in users)) {
     return {
       status: false,
-      reason: "Invalid userId"
+      reason: 'Invalid userId',
     };
   }
   console.log(sessionId);
   if (!sessionDoesExist) {
     return {
       status: false,
-      reason: "Invalid sessionId"
+      reason: 'Invalid sessionId',
     };
   }
-  let username = users[userId]["username"];
-  let itemsListed = users[userId]["itemsListed"];
-  let transactions = users[userId]["transactions"];
-  let description = users[userId]["description"];
+  let username = users[userId]['username'];
+  let itemsListed = users[userId]['itemsListed'];
+  let transactions = users[userId]['transactions'];
+  let description = users[userId]['description'];
 
   if (userId in users && sessionDoesExist) {
     return {
@@ -305,12 +305,12 @@ let user = (userId, sessionId) => {
       username,
       itemsListed,
       transactions,
-      description
+      description,
     };
   }
   return {
     status: false,
-    reason: "unknown error"
+    reason: 'unknown error',
   };
 };
 
@@ -323,5 +323,5 @@ module.exports = {
   buy,
   search,
   addItem,
-  user
+  user,
 };
