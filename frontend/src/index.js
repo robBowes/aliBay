9e0dbd497ace323f20882a96a940f252bf982009
robBowes <32183505+robBowes@users.noreply.github.com>
2018-05-10 11:07:48 -0400

    more preps for redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import {createStore} from 'redux';
 import allReducers from './reducers/index.js';
 import {Provider} from 'react-redux';
 
-const store = createStore(allReducers);
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render((<Provider store={store}><App /></Provider>), document.getElementById('root')
     );
