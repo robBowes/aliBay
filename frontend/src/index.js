df088e7a6d8b5dcb3edfb30b63249a9843981c42
robBowes <32183505+robBowes@users.noreply.github.com>
2018-05-09 21:30:01 -0400

    install redux and make dirs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux';
+import allReducers from './reducers/index.js';
+
+const store = createStore();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
