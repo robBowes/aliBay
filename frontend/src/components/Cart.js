074fdf598da49619a0ddc79e679f6a364f76dba2
phillips-jordan <jordan.phillips.nb@gmail.com>
2018-05-11 17:54:14 -0400

    cart fun

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -3,42 +3,69 @@ import styled from "styled-components";
 import { connect } from "react-redux";
 
 class Cart extends Component {
+    constructor(){
+        super();
+        this.state={
+            cart: []
+        }
+    }
   getCart = () => {
-      console.log(this.props.userId)
-    fetch("/user", {
+    console.log(this.props.userId);
+    return fetch("/user", {
       method: "POST",
       credentials: "same-origin",
-      body: JSON.stringify({userId: parseInt(this.props.userId) })
+      body: JSON.stringify({ userId: parseInt(this.props.userId) })
     })
       .then(res => res.json())
       .then(data => {
-          console.log(data)
-          
-        this.props.dispatch({type: 'UPDATE_CART', payload: data.cart})
-      });
+        console.log(data);
+
+        this.props.dispatch({ type: "UPDATE_CART", payload: data.cart });
+      })
+     ;
+  };
+ 
+  renderCartById = () => {
+      console.log(this.props.items)
+    if(!this.props.items){
+        return
+    }
+    let cartIds = Object.keys(this.props.items)
+    let cartItems = []
+    cartItems = cartIds.map((x,k)=>{
+        return this.props.allItems.filter(y=>{
+            return y.itemId === x
+        })
+    })
+    console.log(cartItems)
+    return cartItems[0];
   };
   trify = (x, k) => {
     return (
       <tr>
         <td>
-          <img style={{ "max-width": "80px" }} src={x.filename} />
+          <img style={{ "max-width": "100px" }} src={x.filename} />
         </td>
         <td>{x.itemName}</td>
         <td>{"$" + x.price}</td>
-        <td>{x.quantity}</td>
+        <td>{this.props.items[x.itemId]}</td>
       </tr>
     );
   };
-  componentDidMount = () => {
-      setTimeout(this.getCart, 200)
+  componentWillMount= ()=>{
+      this.getCart().then(x => {
+        if (this.state.cart.length < 1) {
+          this.setState({ cart: this.renderCartById() });
+        }
+      });
   }
- 
+
   total = x => {
     console.log(x);
     if (x.length >= 1) {
       return x.reduce((a, b) => {
         console.log(a.price, b.price);
-        return a + parseInt(b.price) * parseInt(b.quantity);
+        return a + parseInt(b.price) * parseInt(this.props.items[b.itemId]);
       }, 0);
     }
     return 0;
@@ -59,11 +86,11 @@ class Cart extends Component {
               <th>PRICE</th>
               <th>QUANTITY</th>
             </tr>
-            {this.props.items.map(this.trify)}
+            {this.state.cart?this.state.cart.map(this.trify):null}
           </table>
         </div>
         <button className="btn btn-lg">CHECKOUT</button>
-        <div className="cartTotal">{"$" + this.total(this.props.items)}</div>
+        <div className="cartTotal">{"$" + this.total(this.state.cart)}</div>
       </div>
     );
   }
@@ -71,8 +98,9 @@ class Cart extends Component {
 
 const mapStateToProps = state => ({
   show: state.view.cart,
-  items: state.displayItems,
-  userId: state.user.userId
+  items: state.user.cart,
+  userId: state.user.userId,
+  allItems: state.items
 });
 
 export default connect(mapStateToProps)(Cart);
